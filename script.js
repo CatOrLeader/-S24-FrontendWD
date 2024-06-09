cb5d252e033940fd20961d1222f1e6dfe425c654
Artur <arturmuxutdinov@gmail.com>
2024-06-09 12:16:57 +0300

    impl(hw1)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('change-theme-btn').addEventListener('click', function() {
+document.getElementById('change-theme-btn').addEventListener('click', function () {
     let body = document.body;
     let bio = document.querySelector('.bio');
     let projects = document.querySelector('.projects');
@@ -6,38 +6,33 @@ document.getElementById('change-theme-btn').addEventListener('click', function()
     let socialLinks = document.querySelector('.social-links');
 
     if (body.style.backgroundColor === 'black') {
-        body.style.backgroundColor = '#f4f4f4';
-        body.style.color = '#333';
-        bio.style.backgroundColor = '#fff';
-        bio.style.color = '#333';
-        projects.style.backgroundColor = '#fff';
-        projects.style.color = '#333';
-        socialLinks.style.backgroundColor = '#fff';
-        socialLinks.style.color = '#333';
-        projectCards.forEach(function(card) {
-            card.style.backgroundColor = '#fff';
-            card.style.color = '#333';
+        changeColor(body, 'black', 'white')
+        changeColor(bio, 'black', 'white')
+        changeColor(projects, 'black', 'white')
+        changeColor(socialLinks, 'black', 'white')
+        projectCards.forEach(function (card) {
+            changeColor(card, 'black', 'white')
         });
     } else {
-        body.style.backgroundColor = 'black';
-        body.style.color = 'white';
-        bio.style.backgroundColor = '#333';
-        bio.style.color = 'white';
-        projects.style.backgroundColor = '#333';
-        projects.style.color = 'white';
-        socialLinks.style.backgroundColor = '#333';
-        socialLinks.style.color = 'white';
-        projectCards.forEach(function(card) {
-            card.style.backgroundColor = '#333';
-            card.style.color = 'white';
+        changeColor(body, 'white', 'black')
+        changeColor(bio, 'white', '#333')
+        changeColor(projects, 'white', '#333')
+        changeColor(socialLinks, 'white', '#333')
+        projectCards.forEach(function (card) {
+            changeColor(card, 'white', '#333')
         });
     }
 });
+
+function changeColor(obj, color, backgroundColor) {
+    obj.style.backgroundColor = backgroundColor;
+    obj.style.color = color;
+}
 
 function openProject(projectId) {
     window.open(`${projectId}.html`, '_blank');
 }
 
-particlesJS.load('particles-js', 'particles.json', function() {
+particlesJS.load('particles-js', 'particles.json', function () {
     console.log('particles.json loaded...');
 });
