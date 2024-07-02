eb3cfbe2464aa6499459ed6ee8da85d8fd732446
Artur <arturmuxutdinov@gmail.com>
2024-07-02 10:12:42 +0300

    impl(hw5)

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -2,20 +2,17 @@
 
 import React from 'react';
 import Image from 'next/image';
-import my_photo from "../public/images/me.jpg"
+import my_photo from '../public/images/me.jpg';
 
 const Bio: React.FC = () => (
   <div className="bio">
     <h2>Briefly about me</h2>
     <div className="bio-content">
-      <Image
-        src={my_photo}
-        alt="Me"
-      />
+      <Image src={my_photo} alt="Me" />
       <p>
-        Hi, I`m Mukhutdinov Artur, and I am mostly a backend developer with a lot
-        of pet projects in my pocket. Now my interests are dedicated to frontend
-        development (HTML, CSS, JS/TS, and UX/UI).
+        Hi, I`m Mukhutdinov Artur, and I am mostly a backend developer with a
+        lot of pet projects in my pocket. Now my interests are dedicated to
+        frontend development (HTML, CSS, JS/TS, and UX/UI).
       </p>
     </div>
   </div>
